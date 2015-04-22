63899ce4af133ba7741ee7ebd2193020a926d854
jpoet1291 <jpoet1291@gmail.com>
2015-04-22 15:23:36 -0700

    Azure fix for images and migrations enabled, also more styling added

---
+++ CalvaryOpebBibleWebsite/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace CalvaryOpebBibleWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CalvaryOpebBibleWebsite.DAL.CalvaryContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CalvaryOpebBibleWebsite.DAL.CalvaryContext";
+        }
+
+        protected override void Seed(CalvaryOpebBibleWebsite.DAL.CalvaryContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
